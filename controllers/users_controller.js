e8c93cae4073bc4725d78b5686cab7f43703e239
Chirag <chiragvalecha97@gmail.com>
2021-03-09 23:55:53 +0530

    set up routes and views for sign in and sign up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,17 @@ module.exports.profile = function (req, res) {
     title: "Profile",
   });
 };
+
+//rendering sign up page
+module.exports.singUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "codeial | sign up",
+  });
+};
+
+//rednering sign in page
+module.exports.singIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "codeial | sign in",
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,7 @@ const usersController = require("../controllers/users_controller");
 
 router.get("/profile", usersController.profile);
 
+router.get("/sign-up", usersController.singUp);
+router.get("/sign-in", usersController.singIn);
+
 module.exports = router;
