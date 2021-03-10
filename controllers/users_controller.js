725e20f1156a03113d1aad732d1a0f3b8f354ba3
Chirag <chiragvalecha97@gmail.com>
2021-03-10 13:25:28 +0530

    Set up sign in session using cookies

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,20 @@
 const User = require("../models/user");
 
 module.exports.profile = function (req, res) {
-  return res.render("profile", {
-    title: "Profile",
-  });
+  if (req.cookies.user_id) {
+    User.findById(req.cookies.user_id, function (err, user) {
+      if (user) {
+        return res.render("user_profile", {
+          title: "user profile",
+          user: user,
+        });
+      } else {
+        return res.redirect("/users/sign-in");
+      }
+    });
+  } else {
+    return res.redirect("/users/sign-in");
+  }
 };
 
 //rendering sign up page
@@ -47,5 +58,24 @@ module.exports.create = function (req, res) {
 
 // sign in and creating a session for the user
 module.exports.createSession = function (req, res) {
-  //TODO Later
+  //find the user
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("Erroe in finding the user while signing in");
+      return;
+    }
+    //handle user found
+    if (user) {
+      //handle password which don't match
+      if (user.password != req.body.password) {
+        return res.redirect("back");
+      }
+      //handle session creation
+      res.cookie("user_id", user.id);
+      return res.redirect("/users/profile");
+    } else {
+      //handle user not found
+      return res.redirect("back");
+    }
+  });
 };
--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,5 @@ router.get("/sign-up", usersController.singUp);
 router.get("/sign-in", usersController.singIn);
 
 router.post("/create", usersController.create);
+router.post("/create-session", usersController.createSession);
 module.exports = router;
