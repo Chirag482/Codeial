aac27aa3cecc161a56dc4c40dd8db6ff09bc8c91
Chirag <chiragvalecha97@gmail.com>
2021-03-14 23:00:52 +0530

    Show the lists of posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,15 @@
+const Post = require("../models/post");
+
 module.exports.home = function (req, res) {
-  console.log(req.cookies);
-  return res.render("home", {
-    title: "home",
+  Post.find({}, function (err, posts) {
+    if (err) {
+      console.log("error in finding posts from database");
+      return;
+    }
+
+    return res.render("home", {
+      title: "home",
+      posts: posts,
+    });
   });
 };
